2dc404056df96e3c51fb5b514333efa2b8c9a916
Joshua Fogg <joshua.h.fogg@gmail.com>
2022-05-11 17:16:23 +0100

    OctCLA: revised multiplier udpate, more complete

--- octcla-gen.m
+++ octcla-gen.m
@@ -42,8 +42,21 @@ function [imax, xmax] = argmax(x, a=inf, X=1:length(x), tol=1e-10)  % TODO check
     end
 end
 
+% HACK Sometimes we will use S and D to index the full set of assets and sometimes
+% we will only be indexing the free or bounded assets. A hack to let us do that
+% now is this function which lets us convert from one to the other.
 
+function output = subindex(X, Y)
+    % SUBINDEX returns locations of elements of X appearing in Y
+    %
+    % Given index vectors X and Y of some other vector V, return an index
+    % vector of Y giving locations of elements of X that appear in Y.
+    %
+    % See also, ISMEMBER.
 
+    [bool_XinY, ind_XinY] = ismember(X, Y);
+    output = ind_XinY(bool_XinY);
+end
 
 
 % KKT CHECK FUNCTIONS
@@ -104,22 +117,42 @@ function [F, B, w] = starting_solution_gen(mu, lb, ub, S, D, tol=1e-10)
     B = [B, D(D ~= i_free)];
 end
 
+function [ins, lam_ins, b_ins, d] = move_to_bound_gen(mu, covar, invcovarF, lb, ub,
+                                                      F, B, S, D, lam_current, w, KKT)
+   % MOVE_TO_BOUND handle the CLA case where an asset moves to its bound
+    %
+    % TODO write function description
+    %
+    % See also, CALCULATE_TURNINGPOINTS
 
-% TODO this won't work because the indexes sets D and S are for invcovar, not invcovarF
-% will need to make a function which converts D to indexes in Fi
+    % a sole free asset cannot move to bound
+    if length(F) == 1
+        ins = b_ins = d = NA; lam_ins = -inf;
+        return
+    end
 
+    b   = zeros(length(mu), 1);  % b vector
+    lam = zeros(length(mu), 1);  % lambda vector
 
-function output = subindex(X, Y)
-    % given index vectors X and Y of some other vector V, return an index
-    % vector of Y giving locations of elements of X that appear in Y.
-    [bool_XinY, ind_XinY] = ismember(X, Y);
-    output = ind_XinY(bool_XinY);
+    muF          = mu(F);
+    invcovarFmuF = invcovarF*muF;
+    covarFB      = covar(F,B);
 end
 
 
 
-function [gam, del, C] = multiplier_update(F, B, S, D, invcovarF, covarFB, muF, lam_current, tol=1e-10)
-    tol = 1e-10;
+
+
+function [gam, del, C, lambda] = multiplier_update(F, B, S, D, w, invcovarF, covarFB, muF, lam_current, tol=1e-10)
+    % MULTIPLIER_UPDATE handle the CLA case where an asset becomes free
+    % 
+    % TODO work out if this is actually needed or can be simplified away
+    % This was mostly me just working through the full set of multiplier update
+    % equations trying to get to grips with how they would link into each other
+    % in code. I'm not even sure it will actually be functional!
+    %
+    % See also, ???
+
     % want D and S to be indexes in free assets, not indexes in all the assets
     F_D = subindex(D, F);
     F_S = subindex(S, F);
@@ -138,10 +171,6 @@ function [gam, del, C] = multiplier_update(F, B, S, D, invcovarF, covarFB, muF, 
         printf("ERROR! Linear system has no solutions")
     end
 
-    % for x & y also need sub-indexed wB
-    wB_S = w(B)(subindex(S, B));
-    wB_D = w(B)(subindex(D, B));
-
     % column sums of the four quadrants of invcovarF
     eF_DinvcovarF_DD = sum(invcovarF(F_D, F_D), 1);
     eF_DinvcovarF_DS = sum(invcovarF(F_D, F_S), 1);
@@ -149,8 +178,19 @@ function [gam, del, C] = multiplier_update(F, B, S, D, invcovarF, covarFB, muF, 
     eF_SinvcovarF_SS = sum(invcovarF(F_S, F_S), 1);
 
     % components of x & y are reused elsewhere so split up
-    x_p1 = 0.5 - sum(wB_D, 1) + eF_DinvcovarF_DD*(covarFB(F_D, F_S)*wB_S) + eF_DinvcovarF_DD*(covarFB(F_D, F_D)*wB_D);
-    y_p1 = 0.5 - sum(wB_S, 1) + eF_SinvcovarF_SS*(covarFB(F_S, F_S)*wB_S) + eF_SinvcovarF_SS*(covarFB(F_S, F_D)*wB_D);
+    if isempty(B)
+        % if B empty we get massive cancellations
+        x_p1 = 0.5;
+        y_p1 = 0.5;
+    else
+        % for x & y also need sub-indexed wB
+        wB_S = w(B)(subindex(S, B));
+        wB_D = w(B)(subindex(D, B));
+        % first part of x and y when B non-empty
+        x_p1 = 0.5 - sum(wB_D, 1) + eF_DinvcovarF_DD*(covarFB(F_D, F_S)*wB_S) + eF_DinvcovarF_DD*(covarFB(F_D, F_D)*wB_D);
+        y_p1 = 0.5 - sum(wB_S, 1) + eF_SinvcovarF_SS*(covarFB(F_S, F_S)*wB_S) + eF_SinvcovarF_SS*(covarFB(F_S, F_D)*wB_D);
+    end
+
     x_p2 = eF_DinvcovarF_DS*muF(F_S) + eF_DinvcovarF_DD*muF(F_D);
     y_p2 = eF_SinvcovarF_SS*muF(F_S) + eF_SinvcovarF_SD*muF(F_D);
 
@@ -162,6 +202,10 @@ function [gam, del, C] = multiplier_update(F, B, S, D, invcovarF, covarFB, muF, 
     gam = (a*x - b*y)/determinant;
     del = (a*y - c*x)/determinant;
 
+    % NOTE calculating gam, del, x, or y is *not* a pre-requisite for C or
+    % lambda. All we need for those are a, b, c, d and {x,y}_p{1,2}. It's not
+    % a massive computational saving, but it's something.
+
     % derivatives of those updated multipliers with respect to lambda
     dGam_dLam = (b*y_p2 - a*x_p2)/determinant;
     dDel_dLam = (c*x_p2 - a*y_p2)/determinant;
@@ -169,9 +213,51 @@ function [gam, del, C] = multiplier_update(F, B, S, D, invcovarF, covarFB, muF, 
     % entry i of C gives whether free asset i is moving toward its upper or lower bound 
     C = invcovarF * ([repmat(dGam_dLam, size(S)); repmat(dDel_dLam, size(D))] + muF);
 
-    lam_current = determinant* + invcovarF*covarFB*w(B))
+    % lambda update equation is separated into chunk. the first of these chunks
+    % is determined by whether we're considering assets becoming free or moving
+    % to a bound.
+
+    % BEGIN BLOCK
+    % if in moves_to_bound, b(i) determined based on derivative
+    b = zeros(size(w));
+    for i = B
+        if C(i) > 0
+            % asset moving towards its upper bound
+            b(i) = ub(i);
+        elseif C(i) < 0
+            b(i) = lb(i);
+        else  % C(i) == 0
+            % since more than one free variable, C(j) == 0 iff all mu(i) equal
+            continue
+        end
+    end
+    if isempty(B)
+        lam_num_p1 = determinant*b;
+    else
+        lam_num_p1 = determinant*(b + invcovarF*covarFB*w(B));
+    end
+    % END BLOCK
+
+    % BEGIN BLOCK
+    % if in becomes_free function, b(i) = w(i)
+    if isempty(B)
+        lam_num_p1 = determinant*w;
+    else
+        lam_num_p1 = determinant*(w + invcovarF*covarFB*w(B));
+    end
+    % END BLOCK
+
+    lam_num_p2 = [repmat(b*y_p1 - a*x_p1, size(S)); repmat(c*x_p1 - a*y_p1, size(D))];
+    lam_den_p1 = [repmat(b*y_p2 - a*x_p2, size(S)); repmat(c*x_p2 - a*y_p2, size(D))];
+    lam_den_p2 = invcovarF*(lam_den_p1 + determinant*muF);
+    lam_new = (lam_num_p1 + lam_num_p2)/lam_den_p2;
 end
 
+
+
+
+% TEST CODE TESTING
+% =================
 
 mu = [0.2; 0.8; 0.4; 0.3];
 lb = [0.2; 0.2; 0.2; 0.2];
