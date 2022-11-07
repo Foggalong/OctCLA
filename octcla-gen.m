9eecc9cbef29cff455335fcb04050897eb6b0c1d
Joshua Fogg <joshua.h.fogg@gmail.com>
2022-11-07 16:27:57 +0000

    Moved genetics example to own ifle

--- octcla-gen.m
+++ octcla-gen.m
@@ -586,28 +586,3 @@ function ws = calculate_turningpoints_gen(mu, covar, lb, ub, S, D, KKT)
     % only return w2, w3, etc since w0 and w1 coincide  % TODO work out why
     ws = ws(:, 2:end)';
 end
-
-
-% TEST CODE TESTING
-% =================
-
-S  = [1];
-D  = [2,3];
-mu = [1; 5; 2];
-lb = [0.0; 0.0; 0.0];
-ub = [1.0; 1.0; 1.0];
-covar = [
-    1, 0, 0;
-    0, 5, 0;
-    0, 0, 3;
-];
-
-% This has turning points
-% ws =
-%    0.50000   0.50000   0.00000
-%    0.50000   0.18750   0.31250
-
-source('../octcla.m')
-tic
-sols = calculate_turningpoints_gen(mu, covar, lb, ub, S, D)'
-toc
