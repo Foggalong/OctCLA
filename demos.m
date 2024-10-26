43e914a67bb7f1500be1b9d06dd303c8f7eb39fe
Joshua Fogg <joshua.h.fogg@gmail.com>
2024-10-26 16:29:14 +0100

    More sophisticated OCS example Also adds more description to the demonstration header

--- demos.m
+++ demos.m
@@ -1,13 +1,19 @@
 % This file is part of OctCLA, Copyright (c) 2022 Josh Fogg, released
 % under the MIT License. See: https://github.com/Foggalong/OctCLA
 
+% Below we demonstrate using OctCLA to solve a traditional portfolio
+% optimization problem by loading data from a file, followed by an
+% optimal contribution selection problem where input data is defined
+% through in-code variables. The computed turning points are checked
+% both using the KKT conditions and against the known solutions. 
+
 % set output formatting
 format compact
 format long
 
+
 disp("FINANCE CODE DEMO")
-% TODO write a suitable description of this code
-addpath(genpath("finance/"))
+addpath(genpath("finance/"))  % need functions in path
 
 % read data from input file into variables
 data = csvread("demos/data/input.csv");
@@ -16,7 +22,7 @@ lb = data(3,:)';
 ub = data(4,:)';
 covar = data(5:end,:);
 
-% calculate turning points using OctCLA functions
+% calculate turning points using OctCLA finance functions
 tic
 sols = calculate_turningpoints(mu, covar, lb, ub, 3)';
 toc
@@ -39,31 +45,33 @@ end
 
 disp("")  % padding
 
+
 disp("GENETICS CODE DEMO")
-% TODO write a suitable description of this code
-addpath(genpath("genetics/"))
+addpath(genpath("genetics/"))  % need functions in path
 
 % set up the problem manually using variables
-S  = [1];  % MATLAB will make a double, but that's fine
-D  = [2,3];
-mu = [1; 5; 2];
-lb = [0.0; 0.0; 0.0];
-ub = [1.0; 1.0; 1.0];
+S  = [1,2];
+D  = [3,4];
+mu = [4; 1; 3; 2];
+lb = repmat(0.0, 4, 1);
+ub = repmat(1.0, 4, 1);
 covar = [
-    1, 0, 0;
-    0, 5, 0;
-    0, 0, 3;
+    2, 0, 0, 0;
+    0, 1, 0, 0;
+    0, 0, 2, 0;
+    0, 0, 0, 1;
 ];
 
 % true turning points of this problem
 truth = [
-    0.50000, 0.50000, 0.00000;
-    0.50000, 0.18750, 0.31250
+   0.5, 0.0, 0.5, 0.0;
+   0.5, 0.0, 5/18, 2/9;
+   1/6, 1/3, 1/3, 1/6
 ]';
 
 % calculate turning points using OctCLA genetics functions
 tic
-sols = calculate_turningpoints_gen(mu, covar, lb, ub, S, D)';
+sols = calculate_turningpoints_gen(mu, covar, lb, ub, S, D, 3)';
 toc
 
 % return turning points to terminal
@@ -78,4 +86,3 @@ if any(any(abs_error > tol) > 0) then
 else
     disp("Success!")
 end
-
